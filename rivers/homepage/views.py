565cd9e376e9f887d7383ca6b8e3278f60269eb7
Aleksandr <Aleksandr260206@yandex.ru>
2024-11-30 10:49:16 +0300

    add about

--- rivers/homepage/views.py
+++ rivers/homepage/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def homepage_view(request):
-    return render(request, "homepage/home.html", {})
+    return render(request, "base.html", {})
+
+
+def about_view(request):
+    if request.user.is_authenticated:
+        # Сюда же проверку на роль и перенаправление
+        return render(request, "homepage/order_trip.html", {})
+    else:
+        # Ну это заглушка пока что, для проверки авторизации поставить "about_auth.html"
+        return render(request, "homepage/order_trip.html", {})
